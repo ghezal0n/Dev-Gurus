64723ff106cf370f82a9071cca9d83d4648c789b
Raoua-blh <62572730+Raoua-blh@users.noreply.github.com>
2022-02-19 22:48:17 +0100

    integrationSG Site Done

---
+++ .history/src/Form/ServiceGuideType_20220218200539.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ServiceGuide;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class ServiceGuideType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('descrp', TextType::class)
+            ->add('NbHeure', TextType::class)
+            ->add('DateCreation')
+            ->add('prix', TextType::class)
+            ->add('Save',SubmitType::class, ['label'=> 'Create Sevice'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ServiceGuide::class,
+        ]);
+    }
+}
